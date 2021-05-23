c36fb1e2c149a4323384e0a02597698b288e5d12
harshtonde <59639282+harshtonde@users.noreply.github.com>
2021-05-23 22:55:27 +0530

    changes

--- assets/js/photoscroll.js
+++ assets/js/photoscroll.js
@@ -1,112 +1,308 @@
-// var photoScroll = [
-//     {
-//         fullname : "Aakansha Porwal",
-//         imagepath : "assets/img/photoscroll/Aakansha_Porwal,_ATC,_LT_Infotech1.jpg",
-//         placementdetail : "L&T Infotech (2016)", 
-
-//     },
-//     {
-//         fullname : "Aakar Gangrade",
-//         imagepath : "assets/img/photoscroll/Aakar_Gangrade_MITM,FJ-44_TCS1.jpg",
-//         placementdetail : "MITM, TCS (2016)", 
-
-//     },
-//     {
-//         fullname : "Aakash Gupta",
-//         imagepath : "assets/img/photoscroll/Aakash_Gupta_IPS_FJ-Bhaw_TCS1.jpg",
-//         placementdetail : "IPS, TCS (2016)", 
-
-//     },
-//     {
-//         fullname : "Aakansha Porwal",
-//         imagepath : "assets/img/photoscroll/Aakansha_Porwal,_ATC,_LT_Infotech1.jpg",
-//         placementdetail : "L&T Infotech (2016)", 
-
-//     },
-//     {
-//         fullname : "Aakansha_Porwal",
-//         imagepath : "assets/img/photoscroll/Aakansha_Porwal,_ATC,_LT_Infotech1.jpg",
-//         placementdetail : "L&T Infotech, (2016)", 
-
-//     },
-//     {
-//         fullname : "Aakansha_Porwal",
-//         imagepath : "assets/img/photoscroll/Aakansha_Porwal,_ATC,_LT_Infotech1.jpg",
-//         placementdetail : "L&T Infotech, (2016)", 
-
-//     },
-//     {
-//         fullname : "Aakansha_Porwal",
-//         imagepath : "assets/img/photoscroll/Aakansha_Porwal,_ATC,_LT_Infotech1.jpg",
-//         placementdetail : "L&T Infotech, (2016)", 
-
-//     },
-//     {
-//         fullname : "Aakansha_Porwal",
-//         imagepath : "assets/img/photoscroll/Aakansha_Porwal,_ATC,_LT_Infotech1.jpg",
-//         placementdetail : "L&T Infotech, (2016)", 
-
-//     },
-// ]
-var photoScroll = [{"fullname":"Aakansha Porwal ","collegename":"(ATC)","company":"Infosys","imagepath":"assets/img/photoscroll/Aakansha_Porwal,_ATC,_LT_Infotech1.jpg"},
-{"fullname":"Aakar Gangrade","collegename":"(MITM)","company":"TCS","imagepath":"assets/img/photoscroll/Aakar_Gangrade_MITM,FJ-44_TCS1.jpg"},
-{"fullname":"Aakash Gupta","collegename":"(IPS)","company":"TCS","imagepath":"assets/img/photoscroll/Aakash_Gupta_IPS_FJ-Bhaw_TCS1.jpg"},
-{"fullname":"Aakash Soni","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Aakash_Soni_SVITS_FJ-48_TCS1.jpg"},
-{"fullname":"Aayushi Bilala","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Aayushi_Bilala_SVITS_FJ-43_TCS1.jpg"},
-{"fullname":"Aayushi Dixit","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Aayushi_Dixit_SVITS_FJ-47_TCS1.jpg"},
-{"fullname":"Aayushi Jain","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Aayushi_Jain_SVITS_FJ-46_TCS1.jpg"},
-{"fullname":"Achal Gupta","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Achal_Gupta_SVITS_FJ-46_TCS1.jpg"},
-{"fullname":"Aditi Gupta","collegename":"(MIST)","company":"Universal Hunt","imagepath":"assets/img/photoscroll/Aditi_Gupta_MIST_FJ-53_Universal_Hunt1.jpg"},
-{"fullname":"Aditya Pal","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Aditya_Pal_SVITS_FJ-52_TCS1.jpg"},
-{"fullname":"Aditya wankar","collegename":"(ATC)","company":"BBM","imagepath":"assets/img/photoscroll/Aditya_wankar_ATC_BE(CS)_BBM1.jpg"},
-{"fullname":"Agrani Sharma ","collegename":"(SDBCT)","company":"Infotech","imagepath":"assets/img/photoscroll/Agrani_Sharma,_SDBCT,_LT_Infotech1.jpg"},
-{"fullname":"Aishwariya Dixit","collegename":"(TCET)","company":"TCS","imagepath":"assets/img/photoscroll/Aishwariya_Dixit,TCET,FJ-39,TCS1.jpg"},
-{"fullname":"Ashutosh Sharma ","collegename":"(AITR)","company":"EPA","imagepath":"assets/img/photoscroll/Ashutosh_Sharma,_AITR,_EPA,_3_6_LPA.jpg"},
-{"fullname":"Ayushi Sharma ","collegename":"(SVCE)","company":"Cognizant","imagepath":"assets/img/photoscroll/Ayushi_Sharma,_SVCE,_Cognizant,_3_2_LPA.jpg"},
-{"fullname":"Chayanika Chhajlani ","collegename":"(SVCE)","company":"Rel-Tech","imagepath":"assets/img/photoscroll/Chayanika_Chhajlani,_FJ-25,_SVCE,_MAINTEC,REL-TECH1.JPG"},
-{"fullname":"Chinmay Narwane ","collegename":"(PIES)","company":"Beyond Group","imagepath":"assets/img/photoscroll/Chinmay_Narwane,_PIES,_Beyond_Group,_2_2_LPA.jpg"},
-{"fullname":"Chinmay Pol ","collegename":"(SVCE)","company":"Beyond Industrial","imagepath":"assets/img/photoscroll/Chinmay_Pol,_SVCE,_Beyond_Industrial_Project_Solution,_1_8_LPA.jpg"},
-{"fullname":"Deepak Vishvkarma ","collegename":"(AITR)","company":"Yash Technology","imagepath":"assets/img/photoscroll/Deepak_Vishvkarma,_FJ-26,_AITR,_YASH_TECHNOLOGY1.jpg"},
-{"fullname":"Dhwani Thaker ","collegename":"(AITR)","company":"IBM","imagepath":"assets/img/photoscroll/Dhwani_Thaker,_AITR,_IBM,_3_4_LPA.jpg"},
-{"fullname":"Divyani Biloniya ","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Divyani_Biloniya,_SVITS,_TCS,_3_16_LPA.jpg"},
-{"fullname":"Keshav Bundela","collegename":"(IET DAVV)","company":"Wipro","imagepath":"assets/img/photoscroll/Keshav_Bundela_(IET_DAVV)_Wipro_and_Accenture_.jpg"},
-{"fullname":"Kshama Shah","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Kshama_Shah_(SVITS)_TCS.jpg"},
-{"fullname":"Kshitiz Nagar","collegename":"(AITR)","company":"Tudip Technologies","imagepath":"assets/img/photoscroll/Kshitiz_Nagar_(AITR)_Tudip_Techmologies.jpg"},
-{"fullname":"kushal paliwal","collegename":"(IPS)","company":"TCS","imagepath":"assets/img/photoscroll/kushal_paliwal_(IPS)_TCS.JPG"},
-{"fullname":"Lakshya Sinha","collegename":"(AITR)","company":"Capgemini","imagepath":"assets/img/photoscroll/Lakshya_K_Sinha_(AITR_)_CAPGEMINI.jpeg"},
-{"fullname":"Lavina Garoliya","collegename":"(SVITS)","company":"Captial Via","imagepath":"assets/img/photoscroll/Lavina_Garoliya_(SVITS)_Capital_Via.png"},
-{"fullname":"Mahak Verma","collegename":"(AITR)","company":"Tudip Technologies","imagepath":"assets/img/photoscroll/Mahak_Verma_(AITR_)_Tudip_Technologies.jpg"},
-{"fullname":"Manali Sharma","collegename":"(ATC)","company":"Zensar","imagepath":"assets/img/photoscroll/Manali_Sharma_(ATC)_ZENSAR.jpg"},
-{"fullname":"Manali Sharma","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Manali_Sharma_(SVITS)_TCS.jpg"},
-{"fullname":"MANSI CHAURASIA","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/MANSI_CHAURASIA_(SVITS)_TCS.jpg"},
-{"fullname":"Mantasha Siddiqui","collegename":"(IPS)","company":"TCS","imagepath":"assets/img/photoscroll/Mantasha_Siddiqui_(IPS)_XORIANR_,TCS.jpeg"},
-{"fullname":"Mayank Jain","collegename":"(IIPS)","company":"TCS","imagepath":"assets/img/photoscroll/Mayank_Jain_(IIPS)_TCS.jpg"},
-{"fullname":"Meheraz Khan","collegename":"(CDGI)","company":"Zensar","imagepath":"assets/img/photoscroll/Meheraz_Khan_(CDGI)_Zensar_Technologies.jpg"},
-{"fullname":"Mishel Singh","collegename":"(AITR)","company":"Zensar","imagepath":"assets/img/photoscroll/Mishel_Singh_(AITR)_Zensar_Technologies.jpeg"},
-{"fullname":"Saksi Soni","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Saksi_Soni,SVITS,TCS.jpg"},
-{"fullname":"Samina Zaidi","collegename":"(SGSITS)","company":"Infosys","imagepath":"assets/img/photoscroll/Samina_Zaidi_SGSITS_-Infosys.jpg"},
-{"fullname":"Sanket Yadav","collegename":"(MIST)","company":"CSC","imagepath":"assets/img/photoscroll/Sanket_Yadav_MIST_-_CSC.jpg"},
-{"fullname":"Sapan Rajdev","collegename":"(SVCE)","company":"Yash Technology","imagepath":"assets/img/photoscroll/Sapan_Rajdev_SVCE,_Yash_Technology.png"},
-{"fullname":"Saurabh Agrawal ","collegename":"(MITM)","company":"TCS","imagepath":"assets/img/photoscroll/Saurabh_Agrawal,_MITM,_TCS.jpg"},
-{"fullname":"Shalini Pandey ","collegename":"(SVCE)","company":"Mphasis","imagepath":"assets/img/photoscroll/Shalini_Pandey,SVCE,Mphasis.jpg"},
-{"fullname":"Sharad Khandelwal ","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Sharad_Khandelwal,SVITS,Tcs.jpg"},
-{"fullname":"Shikha Khare","collegename":"(IPS)","company":"Jade Global","imagepath":"assets/img/photoscroll/Shikha_Khare_IPS_-_Jade_global.jpg"},
-{"fullname":"Shimoni Maheshwari ","collegename":"(SDBCT) ","company":"Zensar","imagepath":"assets/img/photoscroll/Shimoni_Maheshwari,SDBCT,Zensar.jpeg"},
-{"fullname":"Shivani Kumawat","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Shivani_Kumawat,SVITS,TCS.jpg"},
-{"fullname":"Shivani Rai ","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/Shivani_Rai,SVITS,TCS.jpg"},
-{"fullname":"Shraddha Chourasiya ","collegename":"(AITR)","company":"Harbinger Group","imagepath":"assets/img/photoscroll/Shraddha_Chourasiya,AITR,_Harbinger_Grp.jpg"},
-{"fullname":"shraddha shah","collegename":"(SVITS)","company":"TCS","imagepath":"assets/img/photoscroll/shraddha_shah_SVITS_-_TCS.jpg"},
-{"fullname":"Shreyas Gupta","collegename":"(AITR)","company":"Persistent","imagepath":"assets/img/photoscroll/Shreyas_Gupta_AITR_-_Persistent.jpg"},
-{"fullname":"Shreya Jain","collegename":"(CDGI)","company":"Future  Market Insight","imagepath":"assets/img/photoscroll/Shreya_jain_CDGI_-_Future_market_Insight.jpg"}]
-
-
+var photoScroll = [
+    {
+        fullname:"Aakansha Porwal",
+        collegename:"(ATC)",
+        company:"Infosys",
+        imagepath:"assets/img/photoscroll/Aakansha_Porwal,_ATC,_LT_Infotech1.jpg"
+    },
+    {
+        fullname:"Aakar Gangrade",
+        collegename:"(MITM)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Aakar_Gangrade_MITM,FJ-44_TCS1.jpg"
+    },
+    {
+        fullname:"Aakash Gupta",
+        collegename:"(IPS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Aakash_Gupta_IPS_FJ-Bhaw_TCS1.jpg"
+    },
+    {
+        fullname:"Aakash Soni",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Aakash_Soni_SVITS_FJ-48_TCS1.jpg"
+    },
+    {
+        fullname:"Aayushi Bilala",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Aayushi_Bilala_SVITS_FJ-43_TCS1.jpg"
+    },
+    {
+        fullname:"Aayushi Dixit",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Aayushi_Dixit_SVITS_FJ-47_TCS1.jpg"
+    },
+    {
+        fullname:"Aayushi Jain",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Aayushi_Jain_SVITS_FJ-46_TCS1.jpg"
+    },
+    {
+        fullname:"Achal Gupta",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Achal_Gupta_SVITS_FJ-46_TCS1.jpg"
+    },
+    {
+        fullname:"Aditi Gupta",
+        collegename:"(MIST)",
+        company:"Universal Hunt",
+        imagepath:"assets/img/photoscroll/Aditi_Gupta_MIST_FJ-53_Universal_Hunt1.jpg"
+    },
+    {
+        fullname:"Aditya Pal",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Aditya_Pal_SVITS_FJ-52_TCS1.jpg"
+    },
+    {
+        fullname:"Aditya wankar",
+        collegename:"(ATC)",
+        company:"BBM",
+        imagepath:"assets/img/photoscroll/Aditya_wankar_ATC_BE(CS)_BBM1.jpg"
+    },
+    {
+        fullname:"Agrani Sharma",
+        collegename:"(SDBCT)",
+        company:"Infotech",
+        imagepath:"assets/img/photoscroll/Agrani_Sharma,_SDBCT,_LT_Infotech1.jpg"
+    },
+    {
+        fullname:"Aishwariya Dixit",
+        collegename:"(TCET)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Aishwariya_Dixit,TCET,FJ-39,TCS1.jpg"
+    },
+    {
+        fullname:"Ashutosh Sharma",
+        collegename:"(AITR)",
+        company:"EPA",
+        imagepath:"assets/img/photoscroll/Ashutosh_Sharma,_AITR,_EPA,_3_6_LPA.jpg"
+    },
+    {
+        fullname:"Ayushi Sharma",
+        collegename:"(SVCE)",
+        company:"Cognizant",
+        imagepath:"assets/img/photoscroll/Ayushi_Sharma,_SVCE,_Cognizant,_3_2_LPA.jpg"
+    },
+    {
+        fullname:"Chinmay Narwane",
+        collegename:"(PIES)",
+        company:"Beyond Group",
+        imagepath:"assets/img/photoscroll/Chinmay_Narwane,_PIES,_Beyond_Group,_2_2_LPA.jpg"
+    },
+    {
+        fullname:"Chinmay Pol",
+        collegename:"(SVCE)",
+        company:"Beyond Industrial",
+        imagepath:"assets/img/photoscroll/Chinmay_Pol,_SVCE,_Beyond_Industrial_Project_Solution,_1_8_LPA.jpg"
+    },
+    {
+        fullname:"Deepak Vishvkarma",
+        collegename:"(AITR)",
+        company:"Yash Technology",
+        imagepath:"assets/img/photoscroll/Deepak_Vishvkarma,_FJ-26,_AITR,_YASH_TECHNOLOGY1.jpg"
+    },
+    {
+        fullname:"Dhwani Thaker",
+        collegename:"(AITR)",
+        company:"IBM",
+        imagepath:"assets/img/photoscroll/Dhwani_Thaker,_AITR,_IBM,_3_4_LPA.jpg"
+    },
+    {
+        fullname:"Divyani Biloniya",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Divyani_Biloniya,_SVITS,_TCS,_3_16_LPA.jpg"
+    },
+    {
+        fullname:"Keshav Bundela",
+        collegename:"(IET DAVV)",
+        company:"Wipro",
+        imagepath:"assets/img/photoscroll/Keshav_Bundela_(IET_DAVV)_Wipro_and_Accenture_.jpg"
+    },
+    {
+        fullname:"Kshama Shah",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Kshama_Shah_(SVITS)_TCS.jpg"
+    },
+    {
+        fullname:"Kshitiz Nagar",
+        collegename:"(AITR)",
+        company:"Tudip Technologies",
+        imagepath:"assets/img/photoscroll/Kshitiz_Nagar_(AITR)_Tudip_Techmologies.jpg"
+    },
+    {
+        fullname:"Kushal paliwal",
+        collegename:"(IPS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/kushal_paliwal_(IPS)_TCS.JPG"
+    },
+    {
+        fullname:"Lakshya Sinha",
+        collegename:"(AITR)",
+        company:"Capgemini",
+        imagepath:"assets/img/photoscroll/Lakshya_K_Sinha_(AITR_)_CAPGEMINI.jpeg"
+    },
+    {
+        fullname:"Lavina Garoliya",
+        collegename:"(SVITS)",
+        company:"Captial Via",
+        imagepath:"assets/img/photoscroll/Lavina_Garoliya_(SVITS)_Capital_Via.png"
+    },
+    {
+        fullname:"Mahak Verma",
+        collegename:"(AITR)",
+        company:"Tudip Technologies",
+        imagepath:"assets/img/photoscroll/Mahak_Verma_(AITR_)_Tudip_Technologies.jpg"
+    },
+    {
+        fullname:"Manali Sharma",
+        collegename:"(ATC)",
+        company:"Zensar",
+        imagepath:"assets/img/photoscroll/Manali_Sharma_(ATC)_ZENSAR.jpg"
+    },
+    {
+        fullname:"Manali Sharma",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Manali_Sharma_(SVITS)_TCS.jpg"
+    },
+    {
+        fullname:"MANSI CHAURASIA",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/MANSI_CHAURASIA_(SVITS)_TCS.jpg"
+    },
+    {
+        fullname:"Mantasha Siddiqui",
+        collegename:"(IPS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Mantasha_Siddiqui_(IPS)_XORIANR_,TCS.jpeg"
+    },
+    {
+        fullname:"Mayank Jain",
+        collegename:"(IIPS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Mayank_Jain_(IIPS)_TCS.jpg"
+    },
+    {
+        fullname:"Meheraz Khan",
+        collegename:"(CDGI)",
+        company:"Zensar",
+        imagepath:"assets/img/photoscroll/Meheraz_Khan_(CDGI)_Zensar_Technologies.jpg"
+    },
+    {
+        fullname:"Mishel Singh",
+        collegename:"(AITR)",
+        company:"Zensar",
+        imagepath:"assets/img/photoscroll/Mishel_Singh_(AITR)_Zensar_Technologies.jpeg"
+    },
+    {
+        fullname:"Saksi Soni",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Saksi_Soni,SVITS,TCS.jpg"
+    },
+    {
+        fullname:"Samina Zaidi",
+        collegename:"(SGSITS)",
+        company:"Infosys",
+        imagepath:"assets/img/photoscroll/Samina_Zaidi_SGSITS_-Infosys.jpg"
+    },
+    {
+        fullname:"Sanket Yadav",
+        collegename:"(MIST)",
+        company:"CSC",
+        imagepath:"assets/img/photoscroll/Sanket_Yadav_MIST_-_CSC.jpg"
+    },
+    {
+        fullname:"Sapan Rajdev",
+        collegename:"(SVCE)",
+        company:"Yash Technology",
+        imagepath:"assets/img/photoscroll/Sapan_Rajdev_SVCE,_Yash_Technology.png"
+    },
+    {
+        fullname:"Saurabh Agrawal",
+        collegename:"(MITM)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Saurabh_Agrawal,_MITM,_TCS.jpg"
+    },
+    {
+        fullname:"Shalini Pandey",
+        collegename:"(SVCE)",
+        company:"Mphasis",
+        imagepath:"assets/img/photoscroll/Shalini_Pandey,SVCE,Mphasis.jpg"
+    },
+    {
+        fullname:"Sharad Khandelwal",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Sharad_Khandelwal,SVITS,Tcs.jpg"
+    },
+    {
+        fullname:"Shikha Khare",
+        collegename:"(IPS)",
+        company:"Jade Global",
+        imagepath:"assets/img/photoscroll/Shikha_Khare_IPS_-_Jade_global.jpg"
+    },
+    {
+        fullname:"Shimoni Maheshwari",
+        collegename:"(SDBCT) ",
+        company:"Zensar",
+        imagepath:"assets/img/photoscroll/Shimoni_Maheshwari,SDBCT,Zensar.jpeg"
+    },
+    {
+        fullname:"Shivani Kumawat",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Shivani_Kumawat,SVITS,TCS.jpg"
+    },
+    {
+        fullname:"Shivani Rai",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/Shivani_Rai,SVITS,TCS.jpg"
+    },
+    {
+        fullname:"Shraddha Chourasiya",
+        collegename:"(AITR)",
+        company:"Harbinger Group",
+        imagepath:"assets/img/photoscroll/Shraddha_Chourasiya,AITR,_Harbinger_Grp.jpg"
+    },
+    {
+        fullname:"shraddha shah",
+        collegename:"(SVITS)",
+        company:"TCS",
+        imagepath:"assets/img/photoscroll/shraddha_shah_SVITS_-_TCS.jpg"
+    },
+    {
+        fullname:"Shreyas Gupta",
+        collegename:"(AITR)",
+        company:"Persistent",
+        imagepath:"assets/img/photoscroll/Shreyas_Gupta_AITR_-_Persistent.jpg"
+    },
+    {
+        fullname:"Shreya Jain",
+        collegename:"(CDGI)",
+        company:"Future Market Insight",
+        imagepath:"assets/img/photoscroll/Shreya_jain_CDGI_-_Future_market_Insight.jpg"
+    }
+]
 
 photoScroll.map(data => {
     document.write("<div class=\"item\">");
     document.write(`<img class="wrapper image" src=\" ${data.imagepath} \">`);
     document.write(`<center><a class="wrapper item name">${data.fullname}</a>`);
     document.write(`<a class="wrapper item company">${data.collegename} ${data.company}</a></center></div>`);
-})
+  })
+  
+  
 
 
 
